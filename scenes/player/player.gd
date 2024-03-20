dcc9f6401a1cd512a40b3bec425af85e30a8aa02
Daniel Matilla Pozuelo <dmatilla@izertis.com>
2024-03-20 17:57:57 +0100

    add wall-jump

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,35 +1,61 @@
 extends CharacterBody2D
 
+const SPEED = 100
+const GRAVITY = 64 * 9.8
+const JUMP_HEIGHT = .35
+const CAST_WALL = 10
+const BOUNDING_JUMP = (GRAVITY * JUMP_HEIGHT) / 2
+
 @onready var sprite = $Sprite
 @onready var jump_sound = $JumpSound
-@onready var screen_size = get_viewport_rect().size
-
-const SPEED = 100
-const GRAVITY = 70 * 9.8
-const JUMP = .32
+@onready var dust = $Dust
+@onready var ray_cast = $RayCast
+var is_jump: bool
+var can_move: bool
 
 func motion_ctrl(axis: Vector2):
-	velocity.x = axis.x * SPEED if (axis != Vector2.ZERO) else 0.0
-	pass
+	if can_move:
+		if axis != Vector2.ZERO:
+			velocity.x = axis.x * SPEED
+		else:
+			velocity.x = 0
 
 func animation_ctrl(axis: Vector2):
+	dust.emitting = false
 	if is_on_floor():
+		can_move = true
 		if axis.x == 0:
 			sprite.animation = "idle"
 		else:
+			dust.emitting = true
 			sprite.animation = "run"
-			sprite.flip_h = axis.x != 1
+			if axis.x == 1:
+				sprite.flip_h = false
+				ray_cast.target_position.y = CAST_WALL
+			else:
+				sprite.flip_h = true
+				ray_cast.target_position.y = -CAST_WALL
 	else:
 		if velocity.y < 0:
 			sprite.animation = "jump"
 		else:
 			sprite.animation = "fall"
-	pass
+
+func wall_jump():
+	var collider = ray_cast.get_collider()
+	
+	if collider.is_in_group("wall") and is_jump:
+		jump_sound.play()
+		velocity.y -= BOUNDING_JUMP
+		if sprite.flip_h:
+			velocity.x += BOUNDING_JUMP
+			sprite.flip_h = false
+		else:
+			velocity.x -= BOUNDING_JUMP
+			sprite.flip_h = true
 
 func _ready():
-	sprite.animation = "idle"
 	sprite.play()
-	pass
 
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
@@ -39,12 +65,13 @@ func _physics_process(delta):
 	animation_ctrl(axis)
 	
 	move_and_slide()
-	pass
 	
 func _input(event):
-	var event_jump = event.is_action_pressed("ui_accept")
+	is_jump = event.is_action_pressed("ui_accept")
 	
-	if event_jump and is_on_floor():
-		velocity.y -= GRAVITY * JUMP		
+	if is_on_floor() and is_jump:
 		jump_sound.play()
-	pass
+		velocity.y -= GRAVITY * JUMP_HEIGHT
+	elif ray_cast.is_colliding():
+		can_move = false
+		wall_jump()
