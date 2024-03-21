fe977943f449e1d4110ee3e0de45253547ded3ea
Daniel Matilla Pozuelo <dmatilla@izertis.com>
2024-03-21 18:08:05 +0100

    add grap to wall

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,16 +5,16 @@ const JUMP_HEIGHT = .35
 const GRAVITY = GLOBAL.GRAVITY
 const JUMP_FORCE = GRAVITY * JUMP_HEIGHT
 const JUMP_BOUNDING = JUMP_FORCE / 2
-const CAST_WALL = 10
-const CAST_ENEMY = 22
+const GRAP_FORCE = GRAVITY / 100
 
 @onready var sprite = $Sprite
 @onready var animation_tree = $AnimationTree
 @onready var jump_sound = $Sounds/Jump
 @onready var hit_sound = $Sounds/Hit
 @onready var dust = $Dust
-@onready var ray_wall = $RayWall
-@onready var ray_eneny = $RayEnemy
+@onready var ray_wall = $RayCasts/RayWall
+@onready var ray_eneny = $RayCasts/RayEnemy
+@onready var ray_casts = $RayCasts
 
 var playback: AnimationNodeStateMachinePlayback
 var can_move: bool
@@ -32,7 +32,6 @@ func _physics_process(delta):
 	axis = GLOBAL.get_axis()
  
 	motion_ctrl()
-	direction_ctrl()
 	animation_ctrl()
 	attack_ctrl()
 
@@ -71,16 +70,8 @@ func animation_ctrl():
 	else:
 		playback.travel("jump" if velocity.y < 0 else "fall")
 
-func direction_ctrl():
-	match sprite.flip_h:
-		false:
-			ray_wall.target_position.x = CAST_WALL
-			ray_eneny.target_position.x = CAST_ENEMY
-		true:
-			ray_wall.target_position.x = -CAST_WALL
-			ray_eneny.target_position.x = -CAST_ENEMY
-
 func motion_ctrl():
+	ray_casts.scale.x = -1 if sprite.flip_h else 1
 	if can_move:
 		velocity.x = axis.x * SPEED
 
@@ -100,15 +91,17 @@ func motion_ctrl():
 func wall_jump():
 	var collider = ray_wall.get_collider()
 
-	if collider.is_in_group("wall") and pressed_jump:
-		jump_sound.play()
-		velocity.y -= JUMP_FORCE
-		if sprite.flip_h:
-			velocity.x += JUMP_BOUNDING
-			sprite.flip_h = false
-		else:
-			velocity.x -= JUMP_BOUNDING
-			sprite.flip_h = true
+	if collider.is_in_group("wall"):
+		velocity.y = GRAP_FORCE
+		if pressed_jump:
+			jump_sound.play()
+			velocity.y = -JUMP_FORCE
+			if sprite.flip_h:
+				velocity.x += JUMP_BOUNDING
+				sprite.flip_h = false
+			else:
+				velocity.x -= JUMP_BOUNDING
+				sprite.flip_h = true
 	
 	ray_wall.enabled = false
 
